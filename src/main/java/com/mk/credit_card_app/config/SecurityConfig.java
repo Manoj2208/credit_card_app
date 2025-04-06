57d8d9095eaa70c3e449ff54d4f467ad0f1e236a
ManojKumar M K <manojmk2209@gmail.com>
2025-04-06 19:29:23 +0530

    Merge branch 'develop' of https://github.com/Manoj2208/credit_card_app into develop
    # Conflicts:
    #	src/main/java/com/mk/credit_card_app/config/JwtAuthenticationEntryPoint.java
    #	src/main/java/com/mk/credit_card_app/dto/CardResponse.java

--- src/main/java/com/mk/credit_card_app/config/SecurityConfig.java
+++ src/main/java/com/mk/credit_card_app/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -43,7 +42,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/credit-cards/**").authenticated().requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/api/v1/auth/login", "/api/v1/user/credit-cards").permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/credit-cards/**").authenticated()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**",
+                        "/api/v1/auth/login", "/api/v1/user/credit-cards").permitAll().anyRequest()
+                        .authenticated())
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
